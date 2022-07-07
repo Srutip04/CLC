9deb255c888cbb664546197d8f4b19b5d11e307b
Srutip04 <subuhishruti75@gmail.com>
2022-07-07 12:38:51 +0530

    studentpage

---
+++ client/src/Pages/StudentPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/nav/nav";
+
+const StudentPage = () => {
+  return (
+    <div>
+     <Nav/>
+    </div>
+  );
+}
+
+export default StudentPage;
