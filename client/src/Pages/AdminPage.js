5a95175c137eb32ed5e5b13a47862177693a1461
Srutip04 <subuhishruti75@gmail.com>
2022-07-07 12:38:38 +0530

    adminpage

---
+++ client/src/Pages/AdminPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/nav/nav";
+
+const AdminPage = () => {
+  return (
+    <div>
+     <Nav/>
+    </div>
+  );
+}
+
+export default AdminPage;
