224ab68ef864f9327e15daee3d0030dbe824d8d2
Srutip04 <subuhishruti75@gmail.com>
2022-07-07 14:56:26 +0530

    added application form

--- client/src/Pages/StudentPage.js
+++ client/src/Pages/StudentPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Nav from "../components/nav/nav";
+import StudentTab from "../components/tabs/tabs";
 
 const StudentPage = () => {
   return (
     <div>
      <Nav/>
+     <StudentTab/>
+
     </div>
   );
 }
