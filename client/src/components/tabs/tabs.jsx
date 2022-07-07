8e5ed666d67fab859845c6dddb03560358451483
Srutip04 <subuhishruti75@gmail.com>
2022-07-07 14:55:29 +0530

    created tabs

---
+++ client/src/components/tabs/tabs.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+  Textarea
+} from "@chakra-ui/react";
+
+const StudentTab = () => {
+
+  return (
+    <Tabs isFitted variant="enclosed">
+      <TabList mb="1em">
+        <Tab>Apply</Tab>
+        <Tab>Applied</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            // bg={useColorModeValue("gray.50", "gray.800")}
+          >
+            <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+              <Stack align={"center"}>
+                <Heading fontSize={"4xl"} textAlign={"center"}>
+                  Application Form
+                </Heading>
+              </Stack>
+              <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "gray.700")}
+                boxShadow={"lg"}
+                p={8}
+              >
+                <Stack spacing={4}>
+                  <HStack>
+                    <Box>
+                      <FormControl id="firstName" isRequired>
+                        <FormLabel>First Name</FormLabel>
+                        <Input type="text" />
+                      </FormControl>
+                    </Box>
+                    <Box>
+                      <FormControl id="lastName">
+                        <FormLabel>Last Name</FormLabel>
+                        <Input type="text" />
+                      </FormControl>
+                    </Box>
+                  </HStack>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>ID</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input type="email" />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Message</FormLabel>
+
+                    <Textarea
+                      name="message"
+                      placeholder="Your Message"
+                      rows={6}
+                      resize="none"
+                    />
+                  </FormControl>
+                  <Stack spacing={10} pt={2}>
+                    <Button
+                      type="submit"
+                      loadingText="Submitting"
+                      size="lg"
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </Stack>
+                </Stack>
+              </Box>
+            </Stack>
+          </Flex>
+        </TabPanel>
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export default StudentTab;
