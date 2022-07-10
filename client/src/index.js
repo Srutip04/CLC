e3884f7c077544acb837b3d4e50141290119ff5b
Srutip04 <subuhishruti75@gmail.com>
2022-07-10 20:34:03 +0530

    authprovider

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AuthProvider from "./context/AuthContext";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
+  <AuthProvider>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
-    
-  </React.StrictMode>
+  </AuthProvider>
   </BrowserRouter>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
