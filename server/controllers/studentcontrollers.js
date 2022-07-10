27cba90d079e0c9391eae99b46f7350b1f61e844
Srutip04 <subuhishruti75@gmail.com>
2022-07-10 15:40:10 +0530

    added auth for student login

---
+++ server/controllers/studentcontrollers.js
@@ -0,0 +1,23 @@
+const asyncHandler = require("express-async-handler");
+const Student = require("../model/studentmodel");
+const generateToken = require("../config/token");
+
+const authStudent = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await Student.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      firstname: user.firstname,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { authStudent };
