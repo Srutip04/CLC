3e4cc208c2fd4b0ac0bf0bea902e7d6180b49aec
Srutip04 <subuhishruti75@gmail.com>
2022-07-14 20:30:42 +0530

    admin dashboard

--- client/src/Pages/AdminPage.js
+++ client/src/Pages/AdminPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/nav/nav";
+import AdminDashboard from "../components/dashboard/admin-dashboard";
 
 const AdminPage = () => {
   return (
     <div>
      <Nav/>
+     <AdminDashboard/>
     </div>
   );
 }
