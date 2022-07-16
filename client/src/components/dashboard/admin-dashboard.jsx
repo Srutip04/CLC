6f132169962a79d51543cbd9ffb69c7fa3587e61
AditiRout <aditi120000@gmail.com>
2022-07-16 12:44:10 +0530

    email

--- client/src/components/dashboard/admin-dashboard.jsx
+++ client/src/components/dashboard/admin-dashboard.jsx
@@ -1,6 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import { AuthState } from "../../context/AuthContext";
 
 const AdminDashboard = () => {
+    const {user}=AuthState();
+    const [dta,setDta]=useState();
+    const getDash = async () => {
+        
+        
+        //console.log(user)
+        try {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          };
+          const { data } = await axios
+            .get(`/api/admin/Dashboard`, config)
+          //console.log(user)
+          //console.log(data);
+          setDta(data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+    const decline=async()=>{
+        try {
+            const sender=
+            const config = {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+            };
+            const { data } = await axios
+              .post(`/api/admin/Dashboard/form-decline`, {},config)
+           
+            setDta(data);
+          } 
+
+    }
+    
+      useEffect(() =>{
+        
+    
+        getDash();
+        
+       },[getDash])
     return (
         <div>
         <h1>Admin Dashboard</h1>
